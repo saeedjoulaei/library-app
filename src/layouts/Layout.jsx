ab3ef262923df209cbfb851ca63af782d1d69f23
saeed joulaei <saeedjoulaei86@gmail.com>
2024-02-27 20:51:49 +0330

    layout css added

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,14 +1,15 @@
+import styles from "../layouts/Layout.module.css";
 function Layout({ children }) {
   return (
     <>
-      <header>
+      <header className={styles.header}>
         <h1>Book App</h1>
         <p>
           <a href="https://www.google.com/">Sj-Developer</a> | Front-End
         </p>
       </header>
       {children}
-      <footer>Developed by Saeid with Love ❤️</footer>
+      <footer className={styles.footer}>Developed by Saeid with Love ❤️</footer>
     </>
   );
 }
